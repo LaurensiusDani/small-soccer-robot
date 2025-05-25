349a65838e24af22df7ab2a15fb4ecc4b46224fb
SomeonesDads <nael.i.manurung@gmail.com>
2025-05-25 18:29:51 +0700

    feat: add movement

--- ros2_ws/src/small_robot_control/small_robot_control/controller.py
+++ ros2_ws/src/small_robot_control/small_robot_control/controller.py
@@ -1,19 +1,31 @@
 import rclpy
+import time
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
 
 class RobotController(Node):
     def __init__(self):
         super().__init__('robot_controller')
         self.get_logger().info('RobotController node started')
-
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
     def move(self, linear, angular):
-        # TODO: Implement robot movement
-        pass
-
+        msg = Twist()
+        msg.linear.x = linear     # Maju/mundur
+        msg.angular.z = angular   # Kiri/kanan
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Gerak: linear={linear}, angular={angular}")
 def main(args=None):
     rclpy.init(args=args)
     node = RobotController()
-    rclpy.spin(node)
+    try:
+        start_time = time.time()
+        while time.time() - start_time < 5:
+            node.move(0.2, 0.0)  
+            time.sleep(0.1)
+        node.move(0.0, 0.0)  
+    except KeyboardInterrupt:
+        pass
+
     node.destroy_node()
     rclpy.shutdown()
 
